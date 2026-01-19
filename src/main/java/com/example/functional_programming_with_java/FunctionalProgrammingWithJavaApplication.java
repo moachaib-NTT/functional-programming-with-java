d57a636f221378650ea3c60a0bdab5f7099933b7
Mohamed reda Achaib <moachaib@emeal.nttdata.com>
2026-01-19 12:44:45 +0100

    Stream 1 (Methode reference)

--- src/main/java/com/example/functional_programming_with_java/FunctionalProgrammingWithJavaApplication.java
+++ src/main/java/com/example/functional_programming_with_java/FunctionalProgrammingWithJavaApplication.java
@@ -3,11 +3,29 @@ package com.example.functional_programming_with_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class FunctionalProgrammingWithJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunctionalProgrammingWithJavaApplication.class, args);
+
+
+		printAllNumbersinListFunctional(List.of(12,9,8,7,6,5,4,3,2,1));
 	}
+
+	public static void print(int number){
+		System.out.println(number);
+	}
+
+	private static void printAllNumbersinListFunctional(List<Integer> numbers) {
+
+		numbers.stream()
+				.forEach(FunctionalProgrammingWithJavaApplication ::print); // Methode Reference
+
+
+	}
+
 
 }
