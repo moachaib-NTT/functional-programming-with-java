2c103d05246f3002a27743daa2bcd42b83af73ac
Mohamed reda Achaib <moachaib@emeal.nttdata.com>
2026-01-19 12:20:36 +0100

    Initial commit: project generated

---
+++ src/main/java/com/example/functional_programming_with_java/FunctionalProgrammingWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.functional_programming_with_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalProgrammingWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalProgrammingWithJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/functional_programming_with_java/FunctionalProgrammingWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.functional_programming_with_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalProgrammingWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
